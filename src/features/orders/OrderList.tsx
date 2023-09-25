9bd76a0c7f1b1dca3e70f1ebed14eb859562a86d
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-09-25 16:13:34 -0300

    Improve menu list

--- src/features/orders/OrderList.tsx
+++ src/features/orders/OrderList.tsx
@@ -1,20 +1,27 @@
-import { Box, Button, IconButton, Paper, Typography } from "@mui/material";
+import { Box, Grid, IconButton, Paper, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import {
   useGetOrdersQuery,
 } from "./orderSlice";
 
+import AddIcon from '@mui/icons-material/Add';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import MenuIcon from '@mui/icons-material/Menu';
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import SearchIcon from '@mui/icons-material/Search';
+import Divider from '@mui/material/Divider';
+import InputBase from '@mui/material/InputBase';
 import { GridFilterModel } from "@mui/x-data-grid";
+import * as React from 'react';
 import { useState } from "react";
-import { OrderTable } from "./components/OrderTable";
 import { Kamban } from "./components/Kamban";
 import { MenuChangeView } from "./components/MenuChangeView";
-import OpenInFullIcon from '@mui/icons-material/OpenInFull';
-import AddIcon from '@mui/icons-material/Add';
+import { OrderTable } from "./components/OrderTable";
 import { QuiltOrderView } from "./components/QuiltOrderView";
+import { TableOpenedOrders } from "./components/TableOpenedOrders";
 
 export const OrderList = () => {
-  const [view, setView] = useState<string>('quilt');
+  const [view, setView] = useState<string>('list');
 
   const handleChange = (event: React.MouseEvent<HTMLElement>, nextView: string) => {
     setView(nextView);
@@ -57,6 +64,8 @@ export const OrderList = () => {
         return (<Kamban />);
       case 'quilt':
         return (<QuiltOrderView />);
+      case 'table':
+        return (<TableOpenedOrders/>);
       default:
         return (<OrderTable
           data={data}
@@ -71,20 +80,40 @@ export const OrderList = () => {
   }
   return (
     <Box>
-      <Box sx={{ justifyContent: 'flex-end', mb: 4, backgroundColor: "FF0000", display: 'flex' }}>
-        <Box>
-          <IconButton
-            component={Link}
-            to="/orders/create"
-            aria-label="delete" size="large" sx={{ mb: 1, mr: 2 }}>
-            <AddIcon fontSize="inherit" />
-          </IconButton>
-          <IconButton aria-label="delete" size="large" sx={{ mb: 1, mr: 2 }}>
-            <OpenInFullIcon fontSize="inherit" />
-          </IconButton>
-          <MenuChangeView view={view} handleChange={handleChange} />
-        </Box>
-      </Box>
+      <Grid container spacing={3}>
+        <Grid item xl={6} lg={6} md={6} sm={6} xs={12}>
+          <Box sx={{ justifyContent: 'flex-start', mb: 4, display: 'flex' }}>
+            {/* <Paper
+              component="form"
+              sx={{ p: '2px 4px', display: 'flex', alignItems: 'center'}}
+            >
+
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Pesquisar"
+                inputProps={{ 'aria-label': 'pesquisar' }}
+              />
+              <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            </Paper> */}
+          </Box>
+        </Grid>
+        <Grid item xl={6} lg={6} md={6} sm={6} xs={12}>
+          <Box sx={{ justifyContent: 'flex-end', mb: 4, display: 'flex' }}>
+            {/*<IconButton
+              component={Link}
+              to="/orders/create"
+              aria-label="delete" size="large" sx={{ mb: 1, mr: 2 }}>
+              <AddIcon fontSize="inherit" />
+            </IconButton>
+             <IconButton aria-label="delete" size="large" sx={{ mb: 1, mr: 2 }}>
+              <OpenInFullIcon fontSize="inherit" />
+            </IconButton> */}
+            <MenuChangeView view={view} handleChange={handleChange} />
+          </Box>
+        </Grid>
+      </Grid>
       <ShowLayout layout={view} />
     </Box>
   );

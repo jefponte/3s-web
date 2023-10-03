22499f8337f05195c448ad575fcb1a91ee31d173
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-10-03 00:04:01 -0300

    Translate

---
+++ src/features/polyglot/polyglotSlice.ts
@@ -0,0 +1,36 @@
+import { RootState } from "../../app/store";
+import { createSlice } from "@reduxjs/toolkit";
+import content from '../../content.json';
+
+const defaultLanguage = navigator.language.split("-")[0];
+const isValidLanguage = (lang: string): lang is keyof typeof content => {
+    return content.hasOwnProperty(lang);
+};
+
+const languageToUse = isValidLanguage(defaultLanguage) ? defaultLanguage : 'pt';
+interface PolyglotState {
+    locale: string;
+    phrases: { default: any;[key: string]: any };
+}
+
+const initialState: PolyglotState = {
+    locale: languageToUse,
+    phrases: { default: content[languageToUse], ...content }
+};
+
+const polyglotSlice = createSlice({
+    name: 'polyglot',
+    initialState: initialState,
+    reducers: {
+        setLocale(state, action) {
+            state.locale = action.payload;
+        }
+    },
+});
+
+export const selectLocale = (state: RootState) => state.polyglot.locale;
+export const selectPhrases = (state: RootState) => state.polyglot.phrases;
+
+
+export const { setLocale } = polyglotSlice.actions;
+export default polyglotSlice.reducer;

4bc4b5e595aafc29215d8dac5128b6efb4700a08
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-02-25 10:23:07 -0300

    BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux"
-import App from "./App"
-import { store } from "./app/store"
-import "./index.css"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./app/store";
+import "./index.css";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { BrowserRouter } from "react-router-dom";
+
 
 const container = document.getElementById("root")
 
@@ -15,11 +17,11 @@ if (container) {
   const root = createRoot(container)
 
   root.render(
-    <React.StrictMode>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </React.StrictMode>,
+    </BrowserRouter>,
   )
 } else {
   throw new Error(

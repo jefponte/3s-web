a8e1f4d91765878cd1d9f4d3507148da9b41ac0f
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-02-25 10:17:57 -0300

    Add govbar and react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./App.css"
 import { Counter } from "./features/counter/Counter"
 import { Quotes } from "./features/quotes/Quotes"
-import logo from "./logo.svg"
+
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.

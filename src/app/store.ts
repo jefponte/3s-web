f41067625c0e6468c2ef99d739e80b66158d9136
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-03-10 13:32:59 -0300

    Remove comment

--- src/app/store.ts
+++ src/app/store.ts
@@ -28,36 +28,3 @@ export type AppThunk<ReturnType = void> = ThunkAction<
 >
 
 
-
-// Template Version
-// import type { Action, ThunkAction } from "@reduxjs/toolkit"
-// import { combineSlices, configureStore } from "@reduxjs/toolkit"
-// import { setupListeners } from "@reduxjs/toolkit/query"
-// import { counterSlice } from "../features/counter/counterSlice"
-// import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
-
-
-// const rootReducer = combineSlices(counterSlice, quotesApiSlice)
-
-// export type RootState = ReturnType<typeof rootReducer>
-// export const makeStore = (preloadedState?: Partial<RootState>) => {
-//   const store = configureStore({
-//     reducer: rootReducer,
-//     middleware: getDefaultMiddleware => {
-//       return getDefaultMiddleware().concat(quotesApiSlice.middleware)
-//     },
-//     preloadedState,
-//   })
-//   setupListeners(store.dispatch)
-//   return store
-// }
-
-// export const store = makeStore()
-// export type AppStore = typeof store
-// export type AppDispatch = AppStore["dispatch"]
-// export type AppThunk<ThunkReturnType = void> = ThunkAction<
-//   ThunkReturnType,
-//   RootState,
-//   unknown,
-//   Action
-// >

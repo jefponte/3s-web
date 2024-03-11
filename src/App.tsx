96f6553c77e080fb149515a67639dde7ef26c11a
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-03-11 17:42:13 -0300

    Clean template

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import "./App.css"
-import { Counter } from "./features/counter/Counter"
-import { Quotes } from "./features/quotes/Quotes"
 import logo from "./logo.svg"
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-
-
-        ABCD
-
-      </header>
+      Teste
     </div>
   )
 }

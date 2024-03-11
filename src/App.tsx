34fb190a9d33917371655121214f67b3c0c04d96
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-03-11 17:51:48 -0300

    Add material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import "./App.css"
-import logo from "./logo.svg"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import * as React from 'react';
 
-const App = () => {
+
+const rows: GridRowsProp = [
+  { id: 1, col1: 'Hello', col2: 'World' },
+  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+];
+
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'Column 1', width: 150 },
+  { field: 'col2', headerName: 'Column 2', width: 150 },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      Teste
+    <div style={{ height: 300, width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} />
     </div>
-  )
+  );
 }
 
-export default App
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       Teste
+//     </div>
+//   )
+// }
+
+// export default App

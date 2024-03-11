4559a4f4ad667bb1a3c05f563236e0e58425fda6
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-03-11 16:36:28 -0300

    New clean project

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,25 +1,42 @@
-import { authSlice } from './../features/auth/authSlice';
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import { servicesApiSlice } from "../features/services/serviceSlice";
-import { apiSlice } from "../features/api/apiSlice";
-import polyglotSlice from '../features/polyglot/polyglotSlice';
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+import { counterSlice } from "../features/counter/counterSlice"
+import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
 
+// `combineSlices` automatically combines the reducers using
+// their `reducerPath`s, therefore we no longer need to call `combineReducers`.
+const rootReducer = combineSlices(counterSlice, quotesApiSlice)
+// Infer the `RootState` type from the root reducer
+export type RootState = ReturnType<typeof rootReducer>
 
-export const store = configureStore({
-  reducer: {
-    polyglot: polyglotSlice,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware)
-})
+// The store setup is wrapped in `makeStore` to allow reuse
+// when setting up tests that need the same store config
+export const makeStore = (preloadedState?: Partial<RootState>) => {
+  const store = configureStore({
+    reducer: rootReducer,
+    // Adding the api middleware enables caching, invalidation, polling,
+    // and other useful features of `rtk-query`.
+    middleware: getDefaultMiddleware => {
+      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+    },
+    preloadedState,
+  })
+  // configure listeners using the provided defaults
+  // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+  setupListeners(store.dispatch)
+  return store
+}
 
-export type AppDispatch = typeof store.dispatch
-export type RootState = ReturnType<typeof store.getState>
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
+export const store = makeStore()
+
+// Infer the type of `store`
+export type AppStore = typeof store
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = AppStore["dispatch"]
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
   RootState,
   unknown,
-  Action<string>
+  Action
 >
--- src/features/orders/components/TableOpenedOrders.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const TableOpenedOrders = () => {
-  return (
-    <div>
-        Table openedOrders
-    </div>
-  )
-}
